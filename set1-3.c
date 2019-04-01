5671af5fc7b20eb36f5c2942a87d6d5729e97b66
sonu41198 <46063063+sonu41198@users.noreply.github.com>
2019-04-01 15:49:26 +0530

    Create set1-3.c

---
+++ set1-3.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+
+int main()
+{
+    int a[50],n,i,j,b[50],k=0,temp,f=0;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }  
+    for(i=0;i<n;i++)
+    {
+     if(i==a[i])
+        {
+            b[k]=a[i];
+            k++;
+            f++;
+        }
+    }
+    b[k]='\0';
+    for(i=0;i<k;i++)
+    {
+        for(j=0;j<k;j++)
+        {
+            if(b[i]<b[j])
+            {
+                temp=b[i];
+                b[i]=b[j];
+                b[j]=temp;
+            }
+        }
+    }
+    for(i=0;i<k;i++)
+    {
+        printf("%d ",b[i]);
+    }
+    if(f==0)
+    {
+        printf("-1");
+    }
+    return 0;
+}
